13ef9cdac9cfcd0ef24d2d32a1d778934399a4dc
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-12 16:16:48 +0700

    Create giaoDienMau.py

---
+++ giaoDienMau.py
@@ -0,0 +1,19 @@
+# Code để tạo cửa sổ
+import tkinter as tk
+class MyProject:
+    def __init__(self, root):
+        # Thiết lập cửa sổ màn hình
+        self.root = root
+        # Thiết lập tiêu đề cho cửa sổ
+        self.root.title('Ứng dụng Tkinter OOP')
+        # Thiết lập kích thước của cửa sổ
+        self.root.geometry('400x300')
+
+# Chạy ứng dụng
+if __name__=='__main__':
+    # Tạo cửa sổ chính
+    root = tk.Tk()
+    # Khởi tạo đối tượng MyProject với cửa sổ chính
+    app = MyProject(root)
+    # Cho người dùng điều khiển cửa sổ
+    root.mainloop()
