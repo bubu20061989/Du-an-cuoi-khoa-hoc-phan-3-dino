99da3d668a6bf63dee2d60d64e30a52233e03329
baolam77 <148604741+baolam77@users.noreply.github.com>
2024-11-09 16:33:47 +0700

    dientichhinhcau.py

--- dientichhinhcau.py
+++ dientichhinhcau.py
@@ -1,11 +1,9 @@
-# Code để tạo cửa sổ
-
-
 import tkinter as tk
-from tkinter as messagebox
+from tkinter import messagebox
+
 class MyProject:
     def __init__(self, root):
-         # Thiết lập cửa sổ màn hình
+        # Thiết lập cửa sổ màn hình
         self.root = root
         # Thiết lập tiêu đề cho cửa sổ
         self.root.title('Chương trình Toán học')
@@ -17,16 +15,16 @@ class MyProject:
         self.width = 63
 
 
-        self.lblBankinh = tk.Label(self.root, text = 'Bán kính hình cầu')
+        self.lblBankinh = tk.Label(self.root, text = 'Bán kính ◯')
         self.lblBankinh.place (x=self.x, y = self.y )
         self.etyBankinh = tk.Entry(self.root, width= self.width)
         self.etyBankinh.place (x = self.x, y = self.y + 20)
 
 
-        self.btnTinhDienTichHC = tk.Button (self.root, text='Diện tích bề mặt', command= self.ChucNang1)
-        self.btnTinhDienTichHC.place (x=self.x + 200, y=self.y + 100)
-        self.btnTinhThetichHC = tk.Button (self.root, text='Thể tích', command= self.ChucNang2)
-        self.btnTinhDienTichHC.place (x=self.x + 200, y=self.y + 120)
+        self.btnTinhChuViHT = tk.Button (self.root, text='Chu Vi', command= self.ChucNang1)
+        self.btnTinhChuViHT.place (x=self.x + 200, y=self.y + 100)
+        self.btnTinhDienTichHT = tk.Button (self.root, text='Dien Tich', command= self.ChucNang2)
+        self.btnTinhDienTichHT.place (x=self.x + 250, y=self.y + 100)
 
     def ChucNang1(self):
         Bankinh = float(self.etyBankinh.get())
@@ -34,7 +32,7 @@ class MyProject:
         if  Bankinh =='':
             messagebox.showerror('Error','You have not typed the length or the width')
         else:
-            messagebox.showinfo('Kết quả ',(Bankinh*Bankinh*4*3.141592653589793238462643383279502884197169399375105821))
+            messagebox.showinfo('Kết quả ',(Bankinh*2*3.141592653589793238462643383279502884197169399375105821))
     
     def ChucNang2(self):
         Bankinh = float(self.etyBankinh.get())
@@ -42,7 +40,8 @@ class MyProject:
         if Bankinh =='':
             messagebox.showerror('Error', 'You have not typed the length or the width')
         else:
-            messagebox.showinfo('Kết quả',Bankinh*Bankinh*Bankinh*(4/3)*3.141592653589793238462643383279502884197169399375105821)
+            messagebox.showinfo('Kết quả',Bankinh*Bankinh*3.141592653589793238462643383279502884197169399375105821)
+        
 # Chạy ứng dụng
 if __name__=='__main__':
     # Tạo cửa sổ chính
